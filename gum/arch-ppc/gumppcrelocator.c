01c2e758907d797ac16176245535a11fd0e156a2
dummys <dummys1337@gmail.com>
2022-10-27 17:00:08 +0200

    relocator: finish read_one

--- gum/arch-ppc/gumppcrelocator.c
+++ gum/arch-ppc/gumppcrelocator.c
@@ -8,7 +8,7 @@
 
 #include "gumlibc.h"
 #include "gummemory.h"
-#include "gumppcreader.h"
+//#include "gumppcreader.h"
 
 // TODO
 #include <string.h>
@@ -96,6 +96,85 @@ gum_ppc_relocator_reset (GumPpcRelocator * relocator,
   relocator->inpos = 0;
   relocator->outpos = 0;
 
+  // TODO
+  // end of block
+  // end of input
   relocator->eob = FALSE;
   relocator->eoi = FALSE;
+}
+
+static guint
+gum_ppc_relocator_inpos (GumPpcRelocator * self)
+{
+  return self->inpos % GUM_MAX_INPUT_INSN_COUNT;
+}
+static void
+gum_ppc_relocator_increment_inpos (GumPpcRelocator * self)
+{
+  self->inpos++;
+  g_assert (self->inpos > self->outpos);
+}
+
+static void
+gum_ppc_relocator_increment_outpos (GumPpcRelocator * self)
+{
+  self->outpos++;
+  g_assert (self->outpos <= self->inpos);
+}
+
+guint
+gum_ppc_relocator_read_one (GumPpcRelocator * self,
+                            const cs_insn ** instruction)
+{
+  cs_insn ** insn_ptr, * insn;
+  const uint8_t * code;
+  size_t size;
+  uint64_t address;
+
+  if (self->eoi)
+    return 0;
+
+  insn_ptr = &self->input_insns[gum_ppc_relocator_inpos (self)];
+
+  if (*insn_ptr == NULL)
+    *insn_ptr = cs_malloc (self->capstone);
+
+  code = self->input_cur;
+  size = 4;
+  address = self->input_pc;
+  insn = *insn_ptr;
+
+  if (!cs_disasm_iter (self->capstone, &code, &size, &address, insn))
+    return 0;
+
+  switch (insn->id)
+  {
+    // unconditional branch: eob + eoi
+    // conditional branch/branch with link: eob  
+
+    /* Here we are defining which instruction do an end of block */
+    /* ret like instructions */
+    case PPC_INS_BL:
+    case PPC_INS_BLR:
+    case PPC_INS_BCL:
+    case PPC_INS_BCLR:
+    case PPC_INS_BCCTRL:
+      self->eob = TRUE;
+      break;
+     
+    case PPC_INS_B:
+      self->eob = TRUE;
+      self->eoi = TRUE;
+      break;
+  }
+
+  gum_ppc_relocator_increment_inpos (self);
+
+  if (instruction != NULL)
+    *instruction = insn;
+
+  self->input_cur = code;
+  self->input_pc = address;
+
+  return self->input_cur - self->input_start;
 }
